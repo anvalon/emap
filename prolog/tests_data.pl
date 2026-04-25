df8d28cd0620abafad51f3706dd4949f7efe1b12
anvalon <a.vaccari@gmail.com>
2026-04-25 22:39:13 +0200

    Updated the tests

--- prolog/tests_data.pl
+++ prolog/tests_data.pl
@@ -19,35 +19,107 @@
 test_case(blank, _, _).
 test_case(info, "NOUNS & INFLECTIONS", _).
 % ---
-test_case(pos, "domoj", 'dom-o-j').
+test_case(pos, "domo",   'dom-o-s').
+test_case(pos, "domon",  'dom-o-n').
+test_case(pos, "domoj",  'dom-o-j').
 test_case(pos, "domojn", 'dom-o-j-n').
-test_case(pos, "domon", 'dom-o-n').
-test_case(pos, "katojn", 'kat-o-j-n').
-test_case(pos, "interpreto", 'interpret-o').
-test_case(pos, "komento", 'koment-o').
-test_case(pos, "telefono", 'telefon-o').
-test_case(pos, "vidojn", 'vid-o-j-n').
-
-% --- Section: VERBS & TENSES ---
-% Simple verbal forms including finite tenses and infinitives.
-test_case(blank, _, _).
-test_case(info, "VERBS & TENSES", _).
-% ---
-test_case(pos, "esperas", 'esper-as').
-test_case(pos, "manĝas", 'manĝ-as').
-test_case(pos, "vidi", 'vid-i').
-test_case(pos, "revidu", 're-vid-u').
-test_case(pos, "malas", 'mal-as').
 
 % --- Section: ADJECTIVES & ADVERBS ---
 % Adjectival and adverbal forms, including participles used as modifiers.
 test_case(blank, _, _).
 test_case(info, "ADJECTIVES & ADVERBS", _).
 % ---
-test_case(pos, "kurante", 'kur-ant-e').
-test_case(pos, "malametaj", 'mal-am-et-a-j').
-test_case(pos, "manĝajn", 'manĝ-a-j-n').
-test_case(pos, "viditaj", 'vid-it-a-j').
+test_case(pos, "bela",   'bel-a').
+test_case(pos, "belan",  'bel-a-n').
+test_case(pos, "belaj",  'bel-a-j').
+test_case(pos, "belajn", 'bel-a-j-n').
+
+% --- Section: VERBS & TENSES ---
+% Simple verbal forms including finite tenses and infinitives.
+test_case(blank, _, _).
+test_case(info, "VERBS & TENSES", _).
+% ---
+test_case(pos, "dormi",  'dorm-i').
+test_case(pos, "dormas", 'dorm-as').
+test_case(pos, "dormis", 'dorm-is').
+test_case(pos, "dormos", 'dorm-os').
+test_case(pos, "dormus", 'dorm-us').
+test_case(pos, "dormu",  'dorm-u').
+
+% --- Section: PREFIXES ---
+% Words with official prefixes (bo, ĉef, dis, ek, eks, ge, mal, mis, pra, re)
+test_case(blank, _, _).
+test_case(info, "PREFIXES", _).
+% ---
+test_case(pos, "bofrato",   'bo-frat-o').
+test_case(pos, "ĉefurbo",   'ĉef-urb-o').
+test_case(pos, "disigi",    'dis-ig-i').
+test_case(pos, "ekiri",     'ek-ir-i').
+test_case(pos, "eksedzo",   'eks-edz-o').
+test_case(pos, "gepatro",   'ge-patr-o').
+test_case(pos, "malamo",    'mal-am-o').
+test_case(pos, "misskribi", 'mis-skrib-i').
+test_case(pos, "pratempo",  'pra-temp-o').
+test_case(pos, "reveni",    're-ven-i').
+
+% --- Section: MORE PREFIXES ---
+% Words with more official prefixes
+test_case(blank, _, _).
+test_case(info, "MORE PREFIXES", _).
+% ---
+
+% --- Section: SUFFIXES ---
+% Words with official suffixes (ad, an, ant, ar, at, aĉ, aĵ, ĉj, ebl, ec, eg, ej, em, end, er, estr, et, id, ig, iĝ, il, in, ind, ing, int, ism, ist, it, nj, obl, on, ont, op, ot, uj, ul, um)
+test_case(blank, _, _).
+test_case(info, "PREFIXES", _).
+% ---
+test_case(pos, "parolado",   'parol-ad-o').
+test_case(pos, "lingvano",   'lingv-an-o').
+test_case(pos, "ridante",    'rid-ant-e').
+test_case(pos, "vortaro",    'vort-ar-o').
+test_case(pos, "manĝata",    'manĝ-at-a').
+test_case(pos, "domaĉo",     'dom-aĉ-o').
+test_case(pos, "glaciaĵo",   'glaci-aĵ-o').
+test_case(pos, "kredeble",   'kred-ebl-e').
+test_case(pos, "beleco",     'bel-ec-o').
+test_case(pos, "grandega",   'grand-eg-a').
+test_case(pos, "lernejo",    'lern-ej-o').
+test_case(pos, "ridema",     'rid-em-a').
+test_case(pos, "legenda",    'leg-end-a').
+test_case(pos, "testero",    'test-er-o').
+test_case(pos, "urbestro",   'urb-estr-o').
+test_case(pos, "beleta",     'bel-et-a').
+test_case(pos, "katido",     'kat-id-o').
+test_case(pos, "konigi",     'kon-ig-i').
+test_case(pos, "sidiĝi",     'sid-iĝ-i').
+test_case(pos, "fermilo",    'ferm-il-o').
+test_case(pos, "virino",     'vir-in-o').
+test_case(pos, "evitinda",   'evit-ind-a').
+test_case(pos, "ŝraŭbingo",  'ŝraŭb-ing-o').
+test_case(pos, "pasinta",    'pas-int-a').
+test_case(pos, "kubismo",    'kub-ism-o').
+test_case(pos, "instruisto", 'instru-ist-o').
+test_case(pos, "legita",     'leg-it-a').
+test_case(pos, "duoble",     'du-obl-e').
+test_case(pos, "triono",     'tri-on-o').
+test_case(pos, "venonta",    'ven-ont-a').
+test_case(pos, "kvarope",    'kvar-op-e').
+test_case(pos, "legota",     'leg-ot-a').
+test_case(pos, "monujo",     'mon-uj-o').
+test_case(pos, "junulo",     'jun-ul-o').
+test_case(pos, "brakumi",    'brak-um-i').
+
+% --- Section: MORE SUFFIXES ---
+% Words with more official suffixes
+test_case(blank, _, _).
+test_case(info, "MORE SUFFIXES", _).
+% ---
+
+% --- Section: MORE PREFIXES AND SUFFIXES ---
+% Words with more official prefixes and suffixes
+test_case(blank, _, _).
+test_case(info, "MORE PREFIXES AND SUFFIXES", _).
+% ---
 
 % --- Section: COMPLEX DERIVATIONS ---
 % Words with multiple affixes (prefixes, suffixes) and compounds.
