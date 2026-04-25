69681b4d8558ac17ddca26bfe5d70b4d2cd3ca1f
anvalon <a.vaccari@gmail.com>
2026-04-25 22:38:28 +0200

    Replaced the final predicate with the ending predicate for the j and n endings.

--- prolog/tails.pl
+++ prolog/tails.pl
@@ -9,19 +9,16 @@
 % --- Unified definition of grammatical endings ---
 % ending(Characters, Tag)
 ending([i],   inf). % Infinitive
-ending([a,s], pzn). % Present (pzn)
-ending([i,s], prt). % Past/Preterite (prt)
+ending([a,s], pzn). % Present
+ending([i,s], prt). % Past
 ending([o,s], fut). % Future
-ending([u,s], kon). % Conditional (kon)
-ending([u],   vol). % Volitive/Imperative
-ending([o],   sub). % Noun (substantivo)
+ending([u,s], kon). % Conditional
+ending([u],   vol). % Volitive
+ending([o],   sub). % Noun
 ending([a],   adj). % Adjective
 ending([e],   adv). % Adverb
-
-% --- Final markers ---
-% final(Characters, Tag)
-final([j], plu). % Plural
-final([n], aku). % Accusative
+ending([j],   plu). % Plural
+ending([n],   aku). % Accusative
 
 % -----------------------------------------------------------------
 % tail/2: Validates the ending and assigns grammatical tags
@@ -38,20 +35,22 @@ tail(T, [[Chars, Tag]|Rest]) :-
     ending(Chars, Tag),
     member(Tag, [sub, adj]),
     append(Chars, Rem, T),
-    check_final_jn(Rem, Rest).
+    check_jn(Rem, Rest).
 
 % Case 3: Adverbs (accept optional -n for direction)
 tail(T, [[Chars, Tag]|Rest]) :-        
     ending(Chars, Tag),
     Tag == adv,
     append(Chars, Rem, T),
-    check_final_n(Rem, Rest).
+    check_n(Rem, Rest).
 
 % --- Helpers to handle final combinations (-j, -n, -jn) ---
-check_final_jn([], []).
-check_final_jn([j], [[[j], plu]]) :- !.
-check_final_jn([n], [[[n], aku]]) :- !.
-check_final_jn([j, n], [[[j], plu], [[n], aku]]) :- !.
+check_jn([], []).
+check_jn([j], [[[j], Tag]]) :- ending([j], Tag), !.
+check_jn([n], [[[n], Tag]]) :- ending([n], Tag), !.
+check_jn([j, n], [[[j], T1], [[n], T2]]) :- 
+    ending([j], T1), 
+    ending([n], T2), !.
 
-check_final_n([], []).
-check_final_n([n], [[[n], aku]]) :- !.
+check_n([], []).
+check_n([n], [[[n], Tag]]) :- ending([n], Tag), !.
