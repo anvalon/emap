fcd0b06df56ff0d57c5267a2c0e64f03397c9167
anvalon <a.vaccari@gmail.com>
2026-04-26 23:39:47 +0200

    Added code for correlatives

--- prolog/words.pl
+++ prolog/words.pl
@@ -18,14 +18,18 @@
 % --- External Dependencies Declarations ---
 % These declarations prevent "undefined procedure" errors during compilation
 % Inform Prolog that bound_extended/2 is defined elsewhere
-:- discontiguous bound_extended/2. 
+:- discontiguous bound_extended/2.
+:- discontiguous correlative_extended/2.
 
 % Main predicate to validate and segment a word.
 % Now strictly focused on the morphological definition.
-word(CharList, Sols) :-
-    word_head(CharList, Sols).
+word(CharList, Output) :-
+    word_head(CharList, Output).
 
 % Section 0: word_head
 % Dispatches the word head to the appropriate morphological category
-word_head(In, Sols) :-
-    bound_extended(In, Sols).
+word_head(In, Output) :-
+    correlative_extended(In, Output).
+
+word_head(In, Output) :-
+    bound_extended(In, Output).
