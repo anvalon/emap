7e31778170d2d71acf98711f1040f14104195ef4
anvalon <a.vaccari@gmail.com>
2026-04-28 19:23:28 +0200

    Modified the "rad" info into the "kor" info.

--- prolog/corrs.pl
+++ prolog/corrs.pl
@@ -8,7 +8,7 @@
 % Following the updated EBNF, correlatives are now treated as 
 % atomic roots (indivisible units) rather than prefix-suffix pairs.
 %
-% All semantic tags have been unified to 'rad' as per user request.
+% All semantic tags have been unified to 'kor' as per user request.
 %
 % The logic is divided into three categories (a, b, c) based on 
 % their inflectional capabilities:
@@ -22,69 +22,70 @@
 
 % --- External Dependencies Declarations ---
 % These predicates are defined in tails.pl to handle grammatical endings.
+:- discontiguous check_prefixes/2.
 :- discontiguous check_jn/2. 
 :- discontiguous check_n/2.
 :- discontiguous tail/2.
 
 % --- Category A: -iu, -ia (Supports Plural and Accusative) ---
 % These correlatives function like adjectives or pronouns.
-corr_a(['k','i','u'], 'rad'). % kiu
-corr_a(['t','i','u'], 'rad'). % tiu
-corr_a(['ĉ','i','u'], 'rad'). % ĉiu
-corr_a(['i','u'],     'rad'). % iu
-corr_a(['n','e','n','i','u'], 'rad'). % neniu
+corr_a(['k','i','u'], 'kor'). % kiu
+corr_a(['t','i','u'], 'kor'). % tiu
+corr_a(['ĉ','i','u'], 'kor'). % ĉiu
+corr_a(['i','u'],     'kor'). % iu
+corr_a(['n','e','n','i','u'], 'kor'). % neniu
 
-corr_a(['k','i','a'], 'rad'). % kia
-corr_a(['t','i','a'], 'rad'). % tia
-corr_a(['ĉ','i','a'], 'rad'). % ĉia
-corr_a(['i','a'],     'rad'). % ia
-corr_a(['n','e','n','i','a'], 'rad'). % nenia
+corr_a(['k','i','a'], 'kor'). % kia
+corr_a(['t','i','a'], 'kor'). % tia
+corr_a(['ĉ','i','a'], 'kor'). % ĉia
+corr_a(['i','a'],     'kor'). % ia
+corr_a(['n','e','n','i','a'], 'kor'). % nenia
 
 % --- Category B: -io, -ie (Supports Accusative) ---
 % These correlatives function as objects or locations.
-corr_b(['k','i','o'], 'rad'). % kio
-corr_b(['t','i','o'], 'rad'). % tio
-corr_b(['ĉ','i','o'], 'rad'). % ĉio
-corr_b(['i','o'],     'rad'). % io
-corr_b(['n','e','n','i','o'], 'rad'). % nenio
+corr_b(['k','i','o'], 'kor'). % kio
+corr_b(['t','i','o'], 'kor'). % tio
+corr_b(['ĉ','i','o'], 'kor'). % ĉio
+corr_b(['i','o'],     'kor'). % io
+corr_b(['n','e','n','i','o'], 'kor'). % nenio
 
-corr_b(['k','i','e'], 'rad'). % kie
-corr_b(['t','i','e'], 'rad'). % tie
-corr_b(['ĉ','i','e'], 'rad'). % ĉie
-corr_b(['i','e'],     'rad'). % ie
-corr_b(['n','e','n','i','e'], 'rad'). % nenie
+corr_b(['k','i','e'], 'kor'). % kie
+corr_b(['t','i','e'], 'kor'). % tie
+corr_b(['ĉ','i','e'], 'kor'). % ĉie
+corr_b(['i','e'],     'kor'). % ie
+corr_b(['n','e','n','i','e'], 'kor'). % nenie
 
 % --- Category C: -am, -al, -el, -es, -om (Supports Tail) ---
 % These are mostly adverbial but can be extended with grammatical endings.
-corr_c(['k','i','a','m'], 'rad'). % kiam
-corr_c(['t','i','a','m'], 'rad'). % tiam
-corr_c(['ĉ','i','a','m'], 'rad'). % ĉiam
-corr_c(['i','a','m'],     'rad'). % iam
-corr_c(['n','e','n','i','a','m'], 'rad'). % neniam
+corr_c(['k','i','a','m'], 'kor'). % kiam
+corr_c(['t','i','a','m'], 'kor'). % tiam
+corr_c(['ĉ','i','a','m'], 'kor'). % ĉiam
+corr_c(['i','a','m'],     'kor'). % iam
+corr_c(['n','e','n','i','a','m'], 'kor'). % neniam
 
-corr_c(['k','i','a','l'], 'rad'). % kial
-corr_c(['t','i','a','l'], 'rad'). % tial
-corr_c(['ĉ','i','a','l'], 'rad'). % ĉial
-corr_c(['i','a','l'],     'rad'). % ial
-corr_c(['n','e','n','i','a','l'], 'rad'). % nenial
+corr_c(['k','i','a','l'], 'kor'). % kial
+corr_c(['t','i','a','l'], 'kor'). % tial
+corr_c(['ĉ','i','a','l'], 'kor'). % ĉial
+corr_c(['i','a','l'],     'kor'). % ial
+corr_c(['n','e','n','i','a','l'], 'kor'). % nenial
 
-corr_c(['k','i','e','l'], 'rad'). % kiel
-corr_c(['t','i','e','l'], 'rad'). % tiel
-corr_c(['ĉ','i','e','l'], 'rad'). % ĉiel
-corr_c(['i','e','l'],     'rad'). % iel
-corr_c(['n','e','n','i','e','l'], 'rad'). % neniel
+corr_c(['k','i','e','l'], 'kor'). % kiel
+corr_c(['t','i','e','l'], 'kor'). % tiel
+corr_c(['ĉ','i','e','l'], 'kor'). % ĉiel
+corr_c(['i','e','l'],     'kor'). % iel
+corr_c(['n','e','n','i','e','l'], 'kor'). % neniel
 
-corr_c(['k','i','e','s'], 'rad'). % kies
-corr_c(['t','i','e','s'], 'rad'). % ties
-corr_c(['ĉ','i','e','s'], 'rad'). % ĉies
-corr_c(['i','e','s'],     'rad'). % ies
-corr_c(['n','e','n','i','e','s'], 'rad'). % nenies
+corr_c(['k','i','e','s'], 'kor'). % kies
+corr_c(['t','i','e','s'], 'kor'). % ties
+corr_c(['ĉ','i','e','s'], 'kor'). % ĉies
+corr_c(['i','e','s'],     'kor'). % ies
+corr_c(['n','e','n','i','e','s'], 'kor'). % nenies
 
-corr_c(['k','i','o','m'], 'rad'). % kiom
-corr_c(['t','i','o','m'], 'rad'). % tiom
-corr_c(['ĉ','i','o','m'], 'rad'). % ĉiom
-corr_c(['i','o','m'],     'rad'). % iom
-corr_c(['n','e','n','i','o','m'], 'rad'). % neniom
+corr_c(['k','i','o','m'], 'kor'). % kiom
+corr_c(['t','i','o','m'], 'kor'). % tiom
+corr_c(['ĉ','i','o','m'], 'kor'). % ĉiom
+corr_c(['i','o','m'],     'kor'). % iom
+corr_c(['n','e','n','i','o','m'], 'kor'). % neniom
 
 % -----------------------------------------------------------------
 % corr_extended/2: Main dispatch for correlatives
